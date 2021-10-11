b639c3f77cc73491a9e50a44c631a61fbb20a6bd
Gone <erickmarx5@gmail.com>
2021-10-11 17:08:56 -0300

    init

---
+++ src/usuario/usuario.entity.ts
@@ -0,0 +1,36 @@
+import { IsNotEmpty, IsString, IsEmail } from 'class-validator';
+import { IsNomeDeUsuarioUnico } from './is-nome-de-usuario-unico.validator';
+
+export class Usuario {
+  id: number;
+
+  @IsNomeDeUsuarioUnico({
+    message: 'nomeDeUsuario precisa ser único.',
+  })
+  @IsNotEmpty({
+    message: 'nomeDeUsuario é obrigatório.',
+  })
+  @IsString({
+    message: 'nomeDeUsuario precisa ser uma string.',
+  })
+  nomeDeUsuario: string;
+
+  @IsEmail(
+    {},
+    {
+      message: 'email precisa ser um endereço de email válido.',
+    },
+  )
+  email: string;
+
+  @IsNotEmpty({
+    message: 'senha é obrigatório.',
+  })
+  senha: string;
+
+  @IsNotEmpty({
+    message: 'nomeCompleto é obrigatório.',
+  })
+  nomeCompleto: string;
+  dataDeEntrada: Date;
+}
